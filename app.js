5f3406c1c0c9ab016bd8ee0d3bc65af0e0d72632
trey-causey <trey.causey@outlook.com>
2020-09-01 15:49:22 -0400

    added routing, commiting to switch to desktop

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
-var http = require('http');
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 4000;
+
+const bookRouter = express.Router();
+bookRouter.route('/workout')
+    .get((req,res) => {
+        const response = { hello: 'This is my API'};
+        res.json(response);
+    });
+
+app.use('/api', bookRouter);
+
+
+app.get('/', (req, res)=>{
+    res.send('Welcome to my API');
+    });
+
+app.listen(port, ()=>{
+    console.log('listening on: ' + port)
+})
+
+
+
+/*var http = require('http');
 var server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     var message = 'It works!\n',
@@ -6,4 +32,4 @@ var server = http.createServer(function(req, res) {
         response = [message, version].join('\n');
     res.end(response);
 });
-server.listen();
+server.listen();*/
